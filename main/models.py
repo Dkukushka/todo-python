f3791687f7de155cd35ad483676da99f566ddf10
Taazim <dkukushka98@gmail.com>
2021-01-26 19:16:48 +0600

    created books page and added booksShop model and in admin

--- main/models.py
+++ main/models.py
@@ -7,3 +7,14 @@ class ToDo(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     is_closed = models.BooleanField(default=False)
     is_favorite = models.BooleanField(default=False)
+
+class BooksShop(models.Model):
+
+    title = models.CharField(verbose_name ='Название книги',max_length = 100)
+    subtitle = models.CharField(verbose_name ='Альтернативное название',max_length = 100)
+    description = models.CharField(verbose_name ='Описание книги',max_length = 500)
+    price = models.DecimalField(decimal_places=2,verbose_name='Цена',max_digits=10)
+    genre = models.CharField(verbose_name ='Жанр книги',max_length = 200)
+    author = models.CharField(verbose_name ='Автор книги',max_length = 100)
+    year = models.DateField(verbose_name='Год выхода книги')
+    date = models.DateTimeField(auto_now_add = True)
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from .models import ToDo
+from .models import BooksShop
 
 def homepage(request):
     return render(request,"index.html")
@@ -8,8 +9,9 @@ def test(request):
     todo_list = ToDo.objects.all()
     return render(request,"test.html",{"todo_list":todo_list})
 
-def second(request):
-    return HttpResponse("test 2 page")
+def books(request):
+    books_list = BooksShop.objects.all()
+    return render(request,"books.html",{"books_list": books_list})
 
 def third(request):
     return HttpResponse("This is page test3")
